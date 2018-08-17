4033355b6208971aa0d044128d98574e79755c95
Leonid Beder <leonid.beder@gmail.com>
2018-08-17 11:14:24 +0300

    Refactor

--- lib/rediska/command_executor.rb
+++ lib/rediska/command_executor.rb
@@ -1,13 +1,16 @@
 module Rediska
   module CommandExecutor
     def write(command)
-      meffod = command.shift.to_s.downcase.to_sym
+      meffod = command[0].to_s.downcase.to_sym
+      args = command[1..-1]
 
       if in_multi && !(TRANSACTION_COMMANDS.include? meffod) # queue commands
-        queued_commands << [meffod, *command]
+        queued_commands << [meffod, *args]
         reply = 'QUEUED'
+      elsif respond_to?(meffod) && method(meffod).arity.zero?
+        reply = send(meffod)
       elsif respond_to?(meffod)
-        reply = send(meffod, *command)
+        reply = send(meffod, *args)
       else
         raise Redis::CommandError, "ERR unknown command '#{meffod}'"
       end
@@ -16,6 +19,8 @@ module Rediska
         reply = 1
       elsif reply == false
         reply = 0
+      elsif reply.is_a?(Array)
+        reply = reply.map { |r| r == true ? 1 : r == false ? 0 : r }
       end
 
       replies << reply
